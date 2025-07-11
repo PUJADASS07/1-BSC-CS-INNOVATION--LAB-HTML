6aba6015aa3ad367c6c0da17515bbc2f655932d5
PUJADASS07 <24bcs236@kgcas.com>
2025-07-11 10:45:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var x=3;
+let name='js';
+const iscloudy=true;
+console.log(x+""+name+""+iscloudy);
+document.write(name);
+document.write(x);
+document.write(iscloudy);
+function add(a,b)
+{
+    console.log("This is a function"+(a+b));
+}
+add(4,5);
