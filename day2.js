7daa9f2908b47f545ee2699dff05a47f480f6f6a
PUJADASS07 <24bcs236@kgcas.com>
2025-07-18 10:50:49 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,12 @@
+var x=3;
+let name='js';
+const iscloudy="true";
+console.log(x+""+name+""+iscloudy);
+document.write(name);
+document.write(x);
+document.write(iscloudy);
+function add()
+{
+    console.log("welcome+(a+b)");
+}
+add(4,5);
